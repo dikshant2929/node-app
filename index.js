96d5da44859cf5f33aa4c695d0de935270f9174b
dikshant_godara <dikshant.godara@girnarsoft.co.in>
2019-11-11 02:19:04 +0530

    Print Logs for running ports

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/', function(req,res){
 	res.send('hello world!');
 });
 
-app.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+	console.log('Server started !!! Running on port: '+port);
+});
 
 module.exports = app;
